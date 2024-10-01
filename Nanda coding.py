4e08cb0785066657df5a3f68c2351c789d45c154
Nunanad <154375172+Nunanad@users.noreply.github.com>
2024-10-01 12:31:32 +0800

    Add files via upload

---
+++ Nanda coding.py
@@ -0,0 +1,23 @@
+
+def hitung_gaji():
+
+    jam_kerja = int(input("Masukkan jam kerja: "))
+    tarif_kerja = int(input("masukkan tarif kerja: "))
+
+    gaji = jam_kerja * tarif_kerja
+    print("Gaji anda adalah: " , gaji)
+
+    if jam_kerja > 160:
+        bonus = 0.10 * gaji  
+        print("ini adalah gaji ditambah bonus anda",bonus)
+    else:
+        print("anda tidak mendapatkan bonus")
+
+while True:
+    hitung_gaji()
+    print()
+    ulang = input("Apakah anda ingin menghitung ulang gaji? (Y/T): ")
+    if ulang.lower() == "t":
+        break
+print("Tertimakasih")
+    
